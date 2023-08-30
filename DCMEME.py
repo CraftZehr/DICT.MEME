eb893311db31bb291488173ebe8a5bac860475a9
C-Zehr <143616188+CraftZehr@users.noreply.github.com>
2023-08-30 20:40:39 +0700

    Create DCMEME.py
    duh theres none

---
+++ DCMEME.py
@@ -0,0 +1,27 @@
+meme_dict = {
+            "CRINGE": "Sometig thats funny or embarrasinng",
+            "LOL": "Laughing  Out  Loud, Someting Very funy",
+            "XD": "Same as LOL"
+            "ROFL": "rolling on floor laughing (funny but not that much)"
+            "SHEESH": "used to express disbelief or exasperation."
+            "AGGRO": "aggresive"
+            "MDR": "same as LOL But in Français, it means Mont de Rire"
+            "L": "Loser"
+            "W": "Win"
+            "BW": "Big Win"
+            "BL": "Big Lost"
+            "YOLO": "You Only Live Once."
+            "YK": "You Know"
+            "OK": "Oll Korrekt"
+            "GOAT":"Very good"
+            "Lit": "amazing"
+            "TBH": "To Be Honest"
+            }
+            
+if True:
+    input = word("what word does you dont understand CAPITAL LETTERS ONLY! mèmè dicktionary")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+        break
+    else:
+    print("INVALID WORD")
